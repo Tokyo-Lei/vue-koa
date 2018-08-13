7506559b953365b6128be78770c980f6c5f47751
stone <stone@ingtube.com>
2018-08-13 14:00:08 +0800

    vue-awesome-swiper组件的使用

---
+++ src/components/swiper/swiperDefault.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <swiper>
+            <swiper-slide>
+
+            </swiper-slide>
+        </swiper>
+    </div>
+</template>
+
+<script>
+    import 'swiper/dist/css/swiper/css'
+    import {swiper, swiperSlide} from 'vue-awesome-swiper'
+
+    export default {
+        name: "swiperDefault",
+        data() {
+            return {
+                slide: [1, 2, 3, 4, 5, 6]
+            }
+        },
+        components: {swiperSlide, swiper},
+    }
+</script>
+
+<style scoped>
+
+</style>
